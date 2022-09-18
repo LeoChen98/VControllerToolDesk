def7508115fb01e2e39789c32be12bd277cebbcc
Leo Chen <Leo@zhangbudademao.com>
2022-09-18 14:48:53 +0800

    添加项目文件。

---
+++ VControllerToolDesk/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VControllerToolDesk
+{
+    internal class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            if (true)
+            {
+                if(Process.GetProcesses().Where(e => e.ProcessName=="VControllerToolDesk").Count() > 1)
+                {
+                    Environment.Exit(1);
+                }
+            }
+
+
+            App app = new App();
+            app.Run(new MainWindow());
+        }
+    }
+}
